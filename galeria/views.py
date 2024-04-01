5d4a1a70d0648a3e3f4ce79f0b6beea53de0462e
RicardoMaranha <ricardomaranhapro@outlook.com>
2024-03-31 22:33:00 -0300

    admin_implementado

--- galeria/views.py
+++ galeria/views.py
@@ -7,7 +7,7 @@ from galeria.models import Fotografia
 
 def index(request):
 
-    fotografias = Fotografia.objects.all()
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
 
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
@@ -15,3 +15,5 @@ def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
 
+def buscar(request):
+    return render(request, 'galeria/buscar.html')
